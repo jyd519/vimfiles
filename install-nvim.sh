e69fb0fe1be0a1950b8ae70067f84877781705c7
jiyongdong <jiyongdong@ata.net.cn>
2025-06-15 12:36:54 +0800

    fix: nvim installation script on linux

--- install-nvim.sh
+++ install-nvim.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
 set -euo pipefail
 
-OS=`uname -s`
-ARCH=`uname -m`
+FROM="gh"
+URL=https://github.com/neovim/neovim/releases/latest/download
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -h | --help)
+      echo "Usage:"
+      echo "  install-nvim.sh [cn|old|gh]"
+      exit
+      ;;
+    gh)
+      ;;
+    old)
+      URL=https://github.com/neovim/neovim-releases/releases/latest/download
+      ;;
+    cn)
+      FROM="cn"
+      URL=https://ata-yuekao-update.oss-cn-beijing.aliyuncs.com/softwares
+      ;;
+    *)
+      echo "Unknown parameter passed: $1"
+      exit 1
+  esac
+  shift
+done
+
+echo $URL
+
+OS=$(uname -s)
+ARCH=$(uname -m)
 target=/opt/nvim
-pkg=nvim-linux64.tar.gz
+pkg=nvim-linux-${ARCH}.tar.gz
 if [[ "$OS" == "Darwin" ]]; then
   pkg=nvim-macos-${ARCH}.tar.gz
 fi
 
 if [[ ! -f $pkg ]]; then
-  curl -Lf -o "$pkg" https://github.com/neovim/neovim/releases/latest/download/$pkg
+  curl -Lf -o "$pkg" $URL/$pkg
 fi
 
-tar xzf $pkg
+mkdir -p nvim-tmp
+tar xzf $pkg --strip-components=1 -C nvim-tmp
 sudo rm -rf $target
 sudo mkdir -p $target
-
-if [[ "$OS" == "Linux" ]]; then
-  sudo mv ./nvim-linux64/* $target
-else
-  sudo mv ./nvim-macos-${ARCH}/* $target
-fi
+sudo mv ./nvim-tmp/* $target
 
 sudo ln -sf $target/bin/nvim /usr/local/bin/
 rm -rf ./$pkg
+rm -rf ./nvim-tmp
