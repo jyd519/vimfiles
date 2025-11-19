84b893eb08c1e5aa96830e40953f6a64c4ac76ab
jiyongdong <jiyongdong@ata.net.cn>
2025-11-19 18:05:16 +0800

    fix: hydra gitsigns

--- vim/lua/myrc/config/hydra.lua
+++ vim/lua/myrc/config/hydra.lua
@@ -355,7 +355,7 @@ Hydra({
 local git_hint = [[
  _J_: next hunk   _s_: stage hunk        _d_: show deleted   _b_: blame line
  _K_: prev hunk   _u_: undo last stage   _p_: preview hunk   _B_: blame show full
- ^ ^              _S_: stage buffer      ^ ^
+ ^ ^              _S_: stage buffer      ^ ^                 _w_: toggle word diff
  ^
  ^ ^              _<Enter>_: Neogit              _q_: exit
 ]]
